99f13324f11a38de7581962e66730210d108969c
sbx_user1051 <sbx_user1051@169.254.144.189>
2020-03-08 12:38:14 +0000

    Sun Mar 08 12:38:14 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -5,23 +5,29 @@ namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
-        static IDictionary<string, int> prices = new Dictionary<string, int>()
+        static IDictionary<char, int> prices = new Dictionary<char, int>()
         {
-            {"A", 50 },
-            {"B", 30 },
-            { "C", 20},
-            {"D", 15 }
+            {'A', 50 },
+            {'B', 30 },
+            {'C', 20},
+            {'D', 15 }
         };
 
         public static int ComputePrice(string skus)
         {
-            if (prices.ContainsKey(skus))
-                return prices[skus];
-            else
-                return -1;
+            int sum = 0;
+            foreach(var sku in skus)
+            {
+                if (prices.ContainsKey(sku))
+                    sum += prices[sku];
+                else
+                    return -1;
+            }
+            return sum;
         }
     }
 }
+
 
 
 
