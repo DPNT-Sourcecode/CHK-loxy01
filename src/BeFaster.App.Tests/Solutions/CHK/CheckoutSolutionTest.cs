4dacbb56237e3a3c0e5e8484555c05076a448658
sbx_user1051 <sbx_user1051@169.254.144.189>
2020-03-08 14:43:47 +0000

    Sun Mar 08 14:43:47 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -40,9 +40,11 @@ namespace BeFaster.App.Tests.Solutions.CHK
         [TestCase("UUUUUUUU", ExpectedResult = 240)]
         [TestCase("VVVVV", ExpectedResult = 220)]
         [TestCase("V", ExpectedResult = 50)]
+        [TestCase("STXYX", ExpectedResult = 82)]
         public int ComputePrice(string skus)
         {
             return CheckoutSolution.ComputePrice(skus);
         }
     }
 }
+
