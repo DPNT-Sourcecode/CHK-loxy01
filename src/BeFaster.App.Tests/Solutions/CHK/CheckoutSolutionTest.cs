879db479cdfa5e5022b513a535933e7f78cb2301
sbx_user1051 <sbx_user1051@169.254.144.189>
2020-03-08 12:35:14 +0000

    Sun Mar 08 12:35:14 UTC 2020

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -11,16 +11,18 @@ namespace BeFaster.App.Tests.Solutions.CHK
     [TestFixture]
     public class CheckoutSolutionTest
     {
-        [TestCase("A", ExpectedResult =50)]
         [TestCase("Z", ExpectedResult = -1)]
+        [TestCase("A", ExpectedResult =50)]
         [TestCase("B", ExpectedResult = 30)]
         [TestCase("C", ExpectedResult = 20)]
         [TestCase("D", ExpectedResult = 15)]
+        [TestCase("AB", ExpectedResult = 80)]
         public int ComputePrice(string skus)
         {
             return CheckoutSolution.ComputePrice(skus);
         }
     }
 }
+
 
 
--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,23 +1,27 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
+        static IDictionary<string, int> prices = new Dictionary<string, int>()
+        {
+            {"A", 50 },
+            {"B", 30 },
+            { "C", 20},
+            {"D", 15 }
+        };
+
         public static int ComputePrice(string skus)
         {
-            if (skus == "A")
-                return 50;
-            else if (skus == "B")
-                return 30;
-            else if (skus == "C")
-                return 20;
-            else if (skus == "D")
-                return 15;
+            if (prices.ContainsKey(skus))
+                return prices[skus];
             else
                 return -1;
         }
     }
 }
+
 
 
