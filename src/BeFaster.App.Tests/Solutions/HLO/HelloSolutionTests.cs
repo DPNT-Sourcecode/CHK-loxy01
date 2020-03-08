41010d3d41535862dafe21118519908aaa589190
sbx_user1051 <sbx_user1051@169.254.144.189>
2020-03-08 12:14:53 +0000

    Sun Mar 08 12:14:53 UTC 2020

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeFaster.App.Solutions.HLO;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Tests.Solutions.HLO
 {
-    class HelloSolutionTests
+    [TestFixture]
+    public class HelloSolutionTests
     {
+        [TestCase("Dave", ExpectedResult = "Hello Dave")]
+        public string SayHello(string friendsName)
+        {
+            return HelloSolution.Hello(friendsName);
+        }
     }
 }
+
