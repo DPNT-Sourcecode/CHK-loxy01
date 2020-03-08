495c3179f67a4e4698a36fa30e6dd1d5f563c223
sbx_user1051 <sbx_user1051@169.254.144.189>
2020-03-08 13:54:51 +0000

    Sun Mar 08 13:54:51 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -13,7 +13,27 @@ namespace BeFaster.App.Solutions.CHK
             {'C', 20},
             {'D', 15 },
             {'E', 40 },
-            { 'F', 10 }
+            {'F', 10 },
+            {'G', 20},
+            {'H', 10},
+            {'I', 35},
+            {'J', 60},
+            {'K', 80},
+            {'L', 90},
+            {'M', 15},
+            {'N', 40},
+            {'O', 10},
+            {'P', 50},
+            {'Q', 30},
+            {'R', 50},
+            {'S', 30},
+            {'T', 20},
+            {'U', 40},
+            {'V', 50},
+            {'W', 20},
+            {'X', 90},
+            {'Y', 10},
+            {'Z', 50 }
         };
 
         public static int ComputePrice(string skus)
@@ -36,11 +56,11 @@ namespace BeFaster.App.Solutions.CHK
             {
                 var countOfItemE = items['E'];
                 var freeBs = countOfItemE / 2;
-                
+
                 items['B'] = (items['B'] - freeBs) > 0 ? items['B'] - freeBs : 0;
             }
 
-            if(items.ContainsKey('F') && items['F'] >= 3)
+            if (items.ContainsKey('F') && items['F'] >= 3)
             {
                 var countOfF = items['F'];
                 var freeFs = countOfF / 3;
@@ -74,4 +94,5 @@ namespace BeFaster.App.Solutions.CHK
         }
     }
 }
+
 
